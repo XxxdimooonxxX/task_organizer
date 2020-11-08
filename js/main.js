517009622d6afceebe9656305d58609d4e72e907
Dmitry <dima2017.koshelev@yandex.ru>
2020-11-08 22:37:29 +0300

    Update design and code in main.js

--- js/main.js
+++ js/main.js
@@ -5,59 +5,67 @@ var active = document.querySelector(".active");
 var complited = document.querySelector(".complited");
 //Form input:
 var input = document.querySelector("input.inp");
-//Button add:
-var bt = document.querySelector(".add");
 //id_class for search with help "for(){}"
 var id_class = 0;
-//All script tag name
-var all_script = document.querySelectorAll("script");
+//
+var formss = document.querySelector("#inp_1");
 //=================================
 
-//t.onclick = () => {
-bt.onclick = () => {
-	//div1 - block with text and two buttons in block"active"
-	var div1 = document.createElement("div");
-	div1.id = "i_" + id_class;
-	id_class++;
-	
-	//p - text in div1
-	var p = document.createElement("p");
-	p.style.display = "inline-block";
-	
-	//Add "p" in div1 and div1 in ".active"
-	active.appendChild(div1);
-	div1.appendChild(p);
-	
-	//add text in "p"
-	p.innerHTML = input.value;
-	
-	//Add button complited
-	var bt_complite = document.createElement("div");
-	bt_complite.id = "button";
-	div1.appendChild(bt_complite);
-	bt_complite.innerHTML = "Complite";
-	bt_complite.style.display = "inline-block";
-	
-	bt_complite.onclick = () => {
-		div1.removeChild(bt_complite);
-		complited.appendChild(div1);
-	}
-	
-	//add button delete
-	var bt_delete = document.createElement("div");
-	bt_delete.id = "button";
-	div1.appendChild(bt_delete);
-	bt_delete.innerHTML = "Delete";
-	bt_delete.style.display = "inline-block";
-	
-	bt_delete.onclick = () => {
-		//if div1 in active then:
-		if(document.querySelector(".active #"+div1.id)!=null){
-			active.removeChild(div1);
-		}else{
-			complited.removeChild(div1);
+//Cancel send при press enter:
+formss.addEventListener('keydown', function(event) {
+    if(event.keyCode == 13) {
+       event.preventDefault();
+    }
+});
+//----------------------------
+
+//При press enter add task in active:
+input.onkeydown = bt_enter;
+function bt_enter(key){
+	if(key.code == "Enter" && input.value != ""){
+		//div1 - block with text and two buttons in block"active"
+		var div1 = document.createElement("div");
+		div1.id = "task";
+		div1.className = "i_" + id_class;
+		id_class++;
+		
+		//p - text in div1
+		var p = document.createElement("p");
+		p.style.display = "inline-block";
+		
+		//Add "p" in div1 and div1 in ".active"
+		active.appendChild(div1);
+		div1.appendChild(p);
+		
+		//add text from input in "p"
+		p.innerHTML = input.value;
+		
+		//add button delete
+		var bt_delete = document.createElement("div");
+		bt_delete.id = "button";
+		bt_delete.innerHTML = "Delete";
+		bt_delete.style.display = "inline-block";
+		div1.appendChild(bt_delete);
+		
+		bt_delete.onclick = () => {
+			//if div1 in active then:
+			if(document.querySelector(".active ."+div1.className)!=null){
+				active.removeChild(div1);
+			}else{
+				complited.removeChild(div1);
+			}
+		}
+		
+		//Add button complited
+		var bt_complite = document.createElement("div");
+		bt_complite.id = "button";
+		bt_complite.innerHTML = "Complite";
+		bt_complite.style.display = "inline-block";
+		div1.appendChild(bt_complite);
+		
+		bt_complite.onclick = () => {
+			div1.removeChild(bt_complite);
+			complited.appendChild(div1);
 		}
 	}
 }
-
-
